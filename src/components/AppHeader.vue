3a125fbe2ac48ec16a573477cf3a3b6fcf317568
GtDavide01 <davidefiorini01@gmail.com>
2022-11-08 15:20:25 +0100

    header completato

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,129 @@
+<script>
+export default{
+    name: "AppHeader",
+    data(){
+        return{
+            listMenu : [
+                {
+                    title: "CHARACTERS",
+                    href: "/characters",
+                    active: false,
+                },
+                {
+                    title: "COMICS",
+                    href: "/comics",
+                    active: false,
+                },
+                {
+                    title: "MOVIES",
+                    href: "/MOVIES",
+                    active: false,
+                },
+                {
+                    title: "TV",
+                    href: "/tv",
+                    active: false,
+                },
+                {
+                    title: "GAMES",
+                    href: "/games",
+                    active: false,
+                },
+                {
+                    title: "COLLECTIBLES",
+                    href: "/collectibles",
+                    active: false,
+                },
+                {
+                    title: "VIDEOS",
+                    href: "/videos",
+                    active: false,
+                },
+                {
+                    title: "FANS",
+                    href: "/fans",
+                    active: false,
+                },
+                {
+                    title: "NEWS",
+                    href: "/news",
+                    active: false,
+                },
+                {
+                    title: "SHOP",
+                    href: "/shop",
+                    active: false,
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="logodc">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="menudc">
+            <ul>
+                <li v-for="(valore , index ) in listMenu" :key="index">
+                <a :class="{ 'active': valore.active }" href="{{valore.href}}">{{valore.title}}</a>    
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+
+<style lang="scss" scoped >
+@use "../styles/general.scss" as *;
+header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 130px;
+    .logodc{
+        padding: 1rem;
+        display: flex;
+        
+        width: 30%;
+        img{
+            max-width: 100%;
+        }
+    }
+    .menudc{
+        height: 100%;
+        display: flex;
+        width: 70%;
+        justify-content: end;
+        ul{
+            align-items: center;
+            display: flex;
+            list-style: none;
+            li{
+                display: flex;
+                height: 100%;
+                align-items: center;
+                font-size: .8rem;
+                line-height: 100%;
+                padding: 0 1rem;
+                font-weight: 900;
+                &:hover,
+                    &.active{
+                        border-bottom: 3px solid #0282f9;
+                        a{
+                            color: #0282f9;
+                        }
+                    }
+                a{  
+                    line-height: 100%;
+                    text-decoration: none;
+                    color: black;
+                }
+                
+            }
+        }
+    }
+}
+</style>
